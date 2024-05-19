e0a4bf18719cf22558a36b8746c477e9bb929288
ArthurV10 <arthurvms10@gmail.com>
2024-05-19 07:39:50 -0300

    Update roll

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 @onready var animation = $Animation
 ## Movimentação do Player
+## Capacidade do player usar o rolamento
+var rolling : bool = false
+var canRoll = true
 var _move_speed: float = 100
 var _friction : float = 0.8
 var _acceleration: float = 0.4
@@ -34,18 +37,25 @@ func manageSword():
 	
 func _physics_process(delta):
 	var _canMove: bool = !Global.uiNode.visible;
+	rolling = $AnimationPlayer.current_animation in ["RollRight", "RollLeft"]
 	if _canMove:
 		_move()
 		move_and_slide()
 	else:
 		velocity = Vector2.ZERO;
+	if rolling:
+		velocity = _direction * _move_speed * 2
+	# Desativar colisão ao rolar. TODO..alterar para hurt box! S2 
+	var my_coll = get_node("Colision") as CollisionShape2D
+	my_coll.disabled = rolling
 	state_machine()
 	
 func _move() -> void:
-	_direction = Vector2(
-		Input.get_axis("move_left", "move_rigth"),
-		Input.get_axis("move_up", "move_down")
-	)
+	if !rolling:
+		_direction = Vector2(
+			Input.get_axis("move_left", "move_rigth"),
+			Input.get_axis("move_up", "move_down")
+		)
 	if _direction != Vector2.ZERO:
 		velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _acceleration)
 		velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _acceleration)
@@ -59,13 +69,14 @@ func _process(delta):
 	manageFacing()
 	manageSword()
 	# Atacar 
-	if Input.is_action_just_pressed("attack"):
+	if Input.is_action_just_pressed("attack") and !rolling:
 		$AnimationPlayer.play("Attack")
 		
 	# Rolar
-	if Input.is_action_just_pressed("roll"):
+	if Input.is_action_just_pressed("roll") and canRoll:
+		canRoll = false
+		$RollRecoveryTimer.start()
 		var _rollAnimation = "RollRight" if facing > 0 else "RollLeft"
-		
 		$AnimationPlayer.play(_rollAnimation)
 
 func state_machine():
@@ -74,3 +85,6 @@ func state_machine():
 		state = "Run"
 	if animation.name != state: 
 		animation.play(state)
+
+func _on_roll_recovery_timer_timeout():
+	canRoll = true
