efdc6d017dc49e5f973bc12c09c4a3d133710a77
João VIctor <joawrt8520@gmail.com>
2024-05-21 18:52:46 -0300

    feat: leaving button clickable only if it is not empty

--- Inventory/Scripts/inv_ui_slot.gd
+++ Inventory/Scripts/inv_ui_slot.gd
@@ -6,12 +6,17 @@ extends Panel
 ## variável de contorle do texto do amount
 @onready var amount_text: Label = get_node("CenterContainer/Panel/Label");
 
+func _process(delta):
+	pass
+
 ## função para atualizar o ícone na grade do inventário:
 func uppdatIcon(slot: InvSlot) -> void:
 		if not slot.item: # se este espaço estiver vazio
 			amount_text.visible = false;
-			icon_visual.visible = false;
+			icon_visual.visible = false;	
+			$Button.disabled = true
 		else:
+			$Button.disabled = false
 			icon_visual.visible = true;
 			icon_visual.texture = slot.item.texture;
 			if slot.amount > 1:
