1689ae94fa7d6889a974d48760b0d0cfd5dae1aa
João VIctor <joawrt8520@gmail.com>
2024-05-22 01:24:34 -0300

    feat: Functionality created to perform an action when pressing the item

--- Inventory/Scripts/inv_ui_slot.gd
+++ Inventory/Scripts/inv_ui_slot.gd
@@ -6,21 +6,26 @@ extends Panel
 ## variável de contorle do texto do amount
 @onready var amount_text: Label = get_node("CenterContainer/Panel/Label");
 
+## Variaável que guarda o nome o item que foi selecionado:
+var myId = "";
+
 func _process(delta):
 	pass
 
 ## função para atualizar o ícone na grade do inventário:
 func uppdatIcon(slot: InvSlot) -> void:
-		if not slot.item: # se este espaço estiver vazio
-			amount_text.visible = false;
-			icon_visual.visible = false;	
-			$Button.disabled = true
-		else:
-			$Button.disabled = false
-			icon_visual.visible = true;
-			icon_visual.texture = slot.item.texture;
-			if slot.amount > 1:
-				amount_text.visible = true;
-			amount_text.text = str(slot.amount);
+	if not slot.item: # se este espaço estiver vazio
+		myId = "";
+		amount_text.visible = false;
+		icon_visual.visible = false;	
+		$Button.disabled = true
+	else:
+		myId = slot.item.name;
+		$Button.disabled = false
+		icon_visual.visible = true;
+		icon_visual.texture = slot.item.texture;
+		if slot.amount > 1:
+			amount_text.visible = true;
+		amount_text.text = str(slot.amount);
 			
 
--- scripts/Global.gd
+++ scripts/Global.gd
@@ -1,9 +1,15 @@
 extends Node
 
+## Referência para o nó do nível atual
 var levelNode = null
+## Referência para o nó da interface do usuário
 var uiNode = null
+## Referência para o nó do jogador
 var playerNode = null
+## Carrega a cena de transição de fade-in para ser usada posteriormente
 var fadeInScene = preload("res://scenes/fade_in.tscn")
+
+## Dicionário das principais cenas do jogo
 var scenes : Dictionary = {
 	"title" : preload("res://scenes/title_screen.tscn"),
 	"world" : preload("res://scenes/world_1.tscn"),
@@ -15,6 +21,4 @@ func changeScene(newScene : String):
 	var transPatro = fadeInScene.instantiate()
 	transPatro.destinyScene = scenes.get(newScene)
 	add_child(transPatro)
-	
-	
 	
