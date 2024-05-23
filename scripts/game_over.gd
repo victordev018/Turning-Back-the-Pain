9afcc37ee3a2393bcb0d96732a252fb9670c21a0
ArthurV10 <arthurvms10@gmail.com>
2024-05-23 01:25:27 -0300

    feat: Make a new world, update lifebar

--- scripts/game_over.gd
+++ scripts/game_over.gd
@@ -1,8 +1,9 @@
 extends CanvasLayer
 
+func _ready():
+	Global.playerHealth = Global.playerMaxHealth
 func _process(delta):
 	pass
-
 
 func _on_restart_buton_pressed():
 	Global.changeScene("world")
