f5c9ac8f1164833da316054b353b08e1b4a8a4a3
João VIctor <joawrt8520@gmail.com>
2024-05-23 08:50:39 -0300

    feat: solving specific scene starting bug

--- Inventory/Scripts/slotbutton.gd
+++ Inventory/Scripts/slotbutton.gd
@@ -1,23 +1,20 @@
 extends Button
 
-## Slot guarda o inventário do player.
-var slot = ItemManage.invPlay.slots;
 ## Acionado quando alguma caixa de slot for clicada:
 func _on_pressed():
+	## Slot guarda o inventário do player.
+	var slot = ItemManage.invPlay.slots;
 	var nameItem = get_parent().myName;
 	print_rich("cliquei no item: [color=orange]", nameItem," amount: ", get_parent().myAmount);
 	var result:bool = ItemManage.useItem(nameItem);
 	if result:
-		removeItem(nameItem);
+		removeItem(nameItem, slot);
 
 ## Decrementa o item que foi clicado caso o uso tenha sido feito:
-func removeItem(nameItem):
+func removeItem(nameItem, slot):
 	for i in range(slot.size()):
 		if (slot[i].item.name == nameItem):
 			slot[i].amount -= 1;
 			print("amount: ", slot[i].amount)
-			#if slot[i].amount <= 0:
-				#slot[i].item = null;
-				#slot.remove_at(i);
 			get_parent().uppdatIcon(slot[i])
 			return;
