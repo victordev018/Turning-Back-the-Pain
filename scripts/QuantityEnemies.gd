3e0dd6c604be7f6bf247649e2ab678b092eab243
João VIctor <joawrt8520@gmail.com>
2024-05-27 23:33:44 -0300

    refactor: Added message that the portal has been opened when there are no enemies on the scene

--- scripts/QuantityEnemies.gd
+++ scripts/QuantityEnemies.gd
@@ -1,10 +1,19 @@
 extends Node2D
 
+## Variável para acessar o label de quantidade de inimigos
+@onready var textLabelEnemy: Label = get_node("PanelText/Label");
 
 func _process(delta) -> void:
+	updateLabelQuantityEnemy()
+	
+## Função para atualizar o label da quantidade de inimigos
+func updateLabelQuantityEnemy() -> void:
 	if Global.levelNode != null:
 		var _enemiesNode = Global.levelNode.find_child("Enemies")
 		var _quantityEnemies = len(_enemiesNode.get_children())
-		$PanelText/Label.text ="enemies: " + str(int(_quantityEnemies));
+		if _quantityEnemies <= 0:
+			textLabelEnemy.text = "The portal has been opened!"
+			return;
+		textLabelEnemy.text ="Enemies: " + str(int(_quantityEnemies));
 	
 	
